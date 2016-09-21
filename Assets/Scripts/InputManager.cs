a3ef3474f92fffddf3eedf435de360a3e4ed7302
Boothand <boothand@hotmail.com>
2016-09-21 14:34:17 +0200

    Input structure to support several players + AI

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+	protected float accelerate;
+	protected float decelerate;
+	protected float tilt;
+
+	public float Accelerate { get { return accelerate; } }
+	public float Decelerate { get { return decelerate; } }
+	public float Tilt { get { return tilt; } }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : InputManager
+{
+	private string accelString = "Accelerate";
+	private string decelString = "Decelerate";
+
+	private string tiltString = "Tilt";
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		accelerate = Input.GetAxisRaw(accelString);
+		decelerate = Input.GetAxisRaw(decelString);
+		tilt = Input.GetAxisRaw(tiltString);
+	}
+}
