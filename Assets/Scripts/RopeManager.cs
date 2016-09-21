a202f17aedb5fc35a80c2fb7543cc0dd39ab8097
Boothand <boothand@hotmail.com>
2016-09-21 19:33:34 +0200

    Custom rope physics

---
+++ Assets/Scripts/RopeManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RopeManager : MonoBehaviour
+{
+	[SerializeField]
+	private int ropeSegments = 2;
+
+	[SerializeField]
+	private Transform target;
+
+	[SerializeField]
+	[Range(0f, 2f)]
+	private float ropeWidth = 0.4f;
+
+	LineRenderer lineRend;
+
+	void Start ()
+	{
+		lineRend = GetComponent<LineRenderer>();
+		lineRend.SetVertexCount(ropeSegments);
+	}
+	
+	void Update ()
+	{
+		//lineRend.SetWidth(ropeWidth, ropeWidth);
+		lineRend.SetPosition(0, transform.position);
+		lineRend.SetPosition(1, target.position);
+
+		float distance = Vector3.Distance(transform.position, target.position);
+		float tiss = (9 - distance) / 10f;
+		tiss = Mathf.Clamp(tiss, 0.2f, 0.2f + distance);
+		print(tiss);
+		lineRend.SetWidth(tiss, tiss);
+	}
+}
---
+++ Assets/Scripts/RopeVictim.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class RopeVictim : MonoBehaviour
+{
+	[SerializeField]
+	private Transform ropeMaster;
+
+	[SerializeField]
+	private float maxDistance = 5f;
+
+	private Vector3 velocity;
+
+	void Start ()
+	{
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		float someForce = 0.1f;
+
+		Vector3 dirToMaster = (ropeMaster.transform.position - transform.position).normalized;
+		float distanceToMaster = Vector3.Distance(ropeMaster.transform.position, transform.position);
+
+		if (distanceToMaster > maxDistance)
+		{
+			velocity += dirToMaster * someForce;
+
+		}
+
+		velocity.y -= 0.981f * Time.deltaTime;
+		
+		velocity = Vector3.ClampMagnitude(velocity, 1f);
+
+		transform.position += velocity;
+	}
+}
