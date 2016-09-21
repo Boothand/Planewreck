16b7198ba8469b46868f8ff8026471f53984637a
axaka <martinvadseth@gmail.com>
2016-09-21 17:18:35 +0200

    UI to show plane velocity.

---
+++ Assets/UIHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+	public Rigidbody player;
+	public Text velocityText;
+	
+	void Update ()
+	{
+		velocityText.text = player.velocity.ToString();
+	}
+}
