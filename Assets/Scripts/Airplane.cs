549c8f1fbb4b1821b2a97466777f9fcadc31c262
Boothand <boothand@hotmail.com>
2016-09-21 16:18:30 +0200

    Airplane object for now. Such modular.

---
+++ Assets/Scripts/Airplane.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Flightmanager))]
+public class Airplane : MonoBehaviour
+{
+	Flightmanager flightManager;
+
+	void Start ()
+	{
+		flightManager = GetComponent<Flightmanager>();
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
