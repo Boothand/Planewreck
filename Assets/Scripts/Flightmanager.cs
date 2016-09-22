9c291bce99d2f02b434de0f2a04f6311a69053da
Boothand <boothand@hotmail.com>
2016-09-22 03:42:33 +0200

    Tidy up

--- Assets/Scripts/Flightmanager.cs
+++ Assets/Scripts/Flightmanager.cs
@@ -6,20 +6,11 @@ public class Flightmanager : MonoBehaviour
 {
 	private	Rigidbody rb;
 	private InputManager input;
-
-	private float maxIdleSpeed = 1000f;
-
-	private Vector3 direction;
+	
 	private float velocity;
 
-	private float motor;
-	private float accelerationSpeed = 1f;
-	private float decelerationSpeed = 1f;
-
-	private float speedModifier = 700f;
-
 	private float pitchSpeed = 5f;
-	private float rollSpeed = 2.5f;
+	private float rollSpeed = 0.45f;
 
 	[SerializeField]
 	private Transform plane;
@@ -29,18 +20,23 @@ public class Flightmanager : MonoBehaviour
 	[SerializeField]
 	private float minSpeed = 20f;
 
+	private Vector3 movementDir;
+
+	public Vector3 Velocity { get { return transform.forward * velocity; } }
+
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
 		input = GetComponent<InputManager>();
+
+		movementDir = Vector3.right;
 	}
 
-	
-	void Update ()
+	void FightGravity()
 	{
-		//ControlMotor();
-
+		//Compare angle deviation from center
 		float angle = Vector3.Angle(Vector3.right, transform.forward);
+
 		if (rb.velocity.x < 0)
 		{
 			angle = Vector3.Angle(Vector3.left, transform.forward);
@@ -48,8 +44,7 @@ public class Flightmanager : MonoBehaviour
 
 		velocity += Time.deltaTime * 5f;
 
-
-		//Facing up
+		//Gradually go slower uphill, faster downhill
 		if (transform.position.y + transform.forward.y > transform.position.y)
 		{
 			velocity *= 1 - (angle / 90f) * Time.deltaTime;
@@ -58,20 +53,39 @@ public class Flightmanager : MonoBehaviour
 		{
 			velocity *= 1 + (angle / 90f) * Time.deltaTime;
 		}
+	}
 
-		velocity = Mathf.Clamp(velocity, minSpeed, maxSpeed);
-
-		Vector3 newDir = new Vector3(input.Horizontal, input.Vertical, 0f);
-
-		transform.forward = Vector3.Lerp(transform.forward, newDir, Time.deltaTime * pitchSpeed);
-
+	void UpdateMesh()
+	{
+		//Make the plane mesh copy the x and y rotation of this object
 		Vector3 planeRot = plane.eulerAngles;
 		planeRot.x = transform.eulerAngles.x;
-		planeRot.y = Mathf.LerpAngle(planeRot.y, transform.eulerAngles.y, Time.deltaTime * (velocity * 0.25f));
+		planeRot.y = Mathf.LerpAngle(planeRot.y, transform.eulerAngles.y, Time.deltaTime * rollSpeed * (velocity * 0.25f));
 		plane.eulerAngles = planeRot;
 
 		plane.position = transform.position;
+	}
+	
+	void Update ()
+	{
+		FightGravity();
+
+		velocity = Mathf.Clamp(velocity, minSpeed, maxSpeed);
+
+		//Face towards the input direction
+		movementDir = new Vector3(input.Horizontal, input.Vertical);
+		transform.forward = Vector3.Lerp(transform.forward, movementDir, Time.deltaTime * pitchSpeed);
+
+		if (plane)
+		{
+			UpdateMesh();
+		}
 
 		rb.velocity = transform.forward * velocity;
+
+		//Force z position to 0
+		Vector3 pos = transform.position;
+		pos.z = 0f;
+		transform.position = pos;
 	}
 }
