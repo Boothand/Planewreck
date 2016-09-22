55ca447e03e7572513da9e4ca51d93c2edaa7c79
Boothand <boothand@hotmail.com>
2016-09-23 01:40:49 +0200

    Gamemanager's slave. Draws stuff on screen in-game.

---
+++ Assets/Scripts/UI/InGameUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUI : MonoBehaviour
+{
+	[SerializeField]
+	private Text countDownText;
+
+	void Start ()
+	{
+		DisableMiddleScreenText();
+	}
+
+	public void SetMiddleScreenText(string text, bool bestFit = false)
+	{
+		countDownText.enabled = true;
+		
+		countDownText.resizeTextForBestFit = bestFit;
+
+		countDownText.text = text;
+	}
+
+	public void DisableMiddleScreenText()
+	{
+		countDownText.enabled = false;
+	}
+
+	void Update ()
+	{
+		
+	}
+}
