f8913d9f56585b8fcfe927d2e81efd4147898027
Boothand <boothand@hotmail.com>
2016-09-23 01:38:22 +0200

    Unnecessary

--- Assets/Scripts/CameraTarget.cs
+++ Assets/Scripts/CameraTarget.cs
@@ -6,9 +6,4 @@ public class CameraTarget : MonoBehaviour
 	private bool trackable = true;
 
 	public bool Trackable { get { return trackable; } set { trackable = value; } }
-
-	void Start()
-	{
-		print(trackable);
-	}
 }
