c934a681ccc1b4ff7942432f7ac042fe402ca244
Boothand <boothand@hotmail.com>
2016-09-22 20:56:26 +0200

    Elements to be tracked by camera

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,14 @@
+﻿//using System.Collections;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+	private bool trackable = true;
+
+	public bool Trackable { get { return trackable; } set { trackable = value; } }
+
+	void Start()
+	{
+		print(trackable);
+	}
+}
