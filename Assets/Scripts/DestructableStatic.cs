ea754c085231eee6f3fd674c83effd519c47a479
Boothand <boothand@hotmail.com>
2016-09-22 03:40:09 +0200

    Building that can be gradually crumbled on impact

---
+++ Assets/Scripts/DestructableStatic.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class DestructableStatic : MonoBehaviour
+{
+	StructureElement[] elements;
+
+	[SerializeField]
+	private float yThreshold = 1f;
+
+	[SerializeField]
+	private float velocityDamper = 0.25f;
+
+	[SerializeField]
+	private Transform top;
+
+	[SerializeField]
+	private float explosionRange = 0.2f;
+
+	private BoxCollider boxCol;
+
+	void Start ()
+	{
+		elements = GetComponentsInChildren<StructureElement>();
+		boxCol = GetComponent<BoxCollider>();
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Vector3 collisionPoint = col.contacts[0].point;
+
+		float dropAmount = Mathf.Abs(top.position.y - collisionPoint.y) / transform.localScale.y;
+
+		Vector3 boxCenter = boxCol.center;
+		boxCenter.y -= dropAmount;
+		boxCol.center = boxCenter;
+
+		Vector3 topPos = top.position;
+		topPos.y -= dropAmount * transform.localScale.y;
+		top.position = topPos;
+
+		Vector3 velocity = col.relativeVelocity;
+
+		if (col.transform.GetComponent<RopeVictim>())
+		{
+			velocity = col.transform.GetComponent<RopeVictim>().Velocity;
+			velocity.x = -velocity.x;
+		}
+		else if (col.transform.GetComponent<Flightmanager>())
+		{
+			velocity = col.transform.GetComponent<Flightmanager>().Velocity / 60f;
+		}
+
+		foreach (StructureElement element in elements)
+		{
+			if (element.transform.position.y + yThreshold >= collisionPoint.y)
+			{
+				velocity = new Vector3(Random.Range(velocity.x - explosionRange, velocity.x + explosionRange),
+										Random.Range(velocity.y - explosionRange, velocity.y + explosionRange));
+
+				velocity *= velocityDamper;
+
+				element.SetVelocity(velocity);
+			}
+		}
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/StructureElement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class StructureElement : MonoBehaviour
+{
+	private bool active;
+	private Vector3 velocity;
+
+	private Vector3 rotation;
+
+	void Start ()
+	{
+		
+	}
+
+	public void SetVelocity(Vector3 newVelocity)
+	{
+		velocity = newVelocity;
+		rotation = newVelocity;
+		active = true;
+	}
+
+	void Update ()
+	{
+		if (!active)
+		{
+			return;
+		}
+
+		velocity.y -= 0.281f * Time.deltaTime;
+
+		transform.position += velocity;
+		transform.Rotate(velocity * Time.deltaTime);
+	}
+}
