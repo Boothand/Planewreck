1af0e00892388cbd35363c792a9d5010d4d1d008
axaka <martinvadseth@gmail.com>
2016-10-07 22:45:16 +0200

    Players overhead text now faces the camera. Made reusable script for this function.

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FaceCamera : FaceObject
+{
+	protected override void Start()
+	{
+		base.Start();
+		if (!target)
+			target = Camera.main.transform;
+	}
+
+	protected override void LateUpdate()
+	{
+		base.LateUpdate();
+	}
+}
---
+++ Assets/Scripts/FaceObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class FaceObject : MonoBehaviour
+{
+	[SerializeField]
+	protected Transform target;
+
+	[SerializeField]
+	protected bool flipForward = false;
+
+	[SerializeField]
+	protected Transform followObject;
+
+	[SerializeField]
+	protected Vector3 offset;
+
+	protected virtual void Start()
+	{
+
+	}
+
+	protected virtual void LateUpdate()
+	{
+		if (flipForward)
+			transform.forward = transform.position - target.position;
+		else
+			transform.forward = target.position - transform.position;
+
+		if (followObject)
+		{
+			transform.position = followObject.position + offset;
+		}
+
+	}
+}
