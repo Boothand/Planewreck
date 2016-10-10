2663651e9b88a11a7fdc16f59627ea37302b0d85
Boothand <boothand@hotmail.com>
2016-10-10 02:33:08 +0200

    Powerup for changing color

---
+++ Assets/Scripts/PowerPower/P_ColorChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class P_ColorChange : PowerBase
+{
+	[SerializeField] Color color = Color.green;
+	float lerpSpeed = 1f;
+
+	protected override void Start()
+	{
+		base.Start();
+	}
+
+	protected override void OnPowerStart()
+	{
+		StartCoroutine(TweenColor(airplane.color, color));
+		base.OnPowerStart();
+	}
+
+	IEnumerator TweenColor(Color oldColor, Color newColor)
+	{
+		float timer = 0f;
+
+		while (timer < 1f)
+		{
+			Color lerpColor = Color.Lerp(oldColor, newColor, timer);
+			timer += Time.deltaTime * lerpSpeed;
+
+			airplane.SetColor(lerpColor);
+
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	protected override IEnumerator EndPowerRoutine()
+	{
+		yield return StartCoroutine(TweenColor(color, airplane.color));
+	}
+}
