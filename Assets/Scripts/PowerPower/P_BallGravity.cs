871f88fd4ca5eddb2e0c46fc1177c12dda0f714a
Boothand <boothand@hotmail.com>
2016-10-10 02:32:59 +0200

    Powerup for changing ball gravity

---
+++ Assets/Scripts/PowerPower/P_BallGravity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class P_BallGravity : PowerBase
+{
+	WreckingBall ball;
+
+	[SerializeField]
+	Vector3 gravity = new Vector3(0f, 100f, 0f);
+
+	Vector3 oldGravity;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		ball = airplane.GetSmasher();
+		oldGravity = ball.gravity;
+	}
+
+	protected override void Start()
+	{
+		base.Start();		
+	}
+
+	protected override void OnPowerStart()
+	{
+		base.OnPowerStart();
+
+		ball.gravity = gravity;
+	}
+
+	protected override void OnPowerEnd()
+	{
+		base.OnPowerEnd();
+		
+		ball.gravity = oldGravity;
+	}
+}
