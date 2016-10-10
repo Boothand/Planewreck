2e65387859562203cee548d3a0ab9bc3020d6e24
Boothand <boothand@hotmail.com>
2016-10-10 02:32:35 +0200

    Crate powerup example

---
+++ Assets/Scripts/PowerPower/Crate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+	[SerializeField]
+	PowerBase power;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.GetComponent<AirplaneManager>())
+		{
+			AirplaneManager airplane = col.GetComponent<AirplaneManager>();
+			GameObject instance = Instantiate(power.gameObject, airplane.transform);
+
+			Destroy(gameObject);
+		}
+	}
+}
