6218b7cdf4c6c0fb92c4f35f0314145ff3f1a992
Boothand <boothand@hotmail.com>
2016-10-10 05:26:21 +0200

    Testing some juicy effects.

--- Assets/Scripts/PowerPower/Crate.cs
+++ Assets/Scripts/PowerPower/Crate.cs
@@ -6,14 +6,67 @@ public class Crate : MonoBehaviour
 	[SerializeField]
 	PowerBase power;
 
+	bool taken;
+
 	void OnTriggerEnter(Collider col)
 	{
+		if (taken)
+		{
+			return;
+		}
+
+		taken = true;
+
 		if (col.GetComponent<AirplaneManager>())
 		{
 			AirplaneManager airplane = col.GetComponent<AirplaneManager>();
 			GameObject instance = Instantiate(power.gameObject, airplane.transform);
 
-			Destroy(gameObject);
+			StartCoroutine(GetTaken(col.transform));
 		}
+	}
+
+	IEnumerator GetTaken(Transform target)
+	{
+		Material mat = GetComponent<Renderer>().material;
+		Color oldCol = mat.color;
+		float timer = 0f;
+		ParticleSystem particles = GetComponentInChildren<ParticleSystem>();
+		ParticleSystem.Particle[] particle = new ParticleSystem.Particle[7];
+
+		particles.Emit(7);
+
+		while (timer < 1f)
+		{
+			particles.GetParticles(particle);
+
+			if (timer > 0.4f)
+			{
+				for (int i = 0; i < particle.Length; i++)
+				{
+					particle[i].position = Vector3.MoveTowards(particle[i].position, transform.position, timer + 0.4f);
+
+				}
+
+				particles.SetParticles(particle, 7);
+			}
+
+			mat.color = Color.Lerp(oldCol, Color.white, timer);
+			timer += Time.deltaTime * 1f;
+			transform.localScale *= 0.95f;
+			transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * 10f);
+			transform.Rotate(Vector3.forward, Time.deltaTime * 400, Space.World);
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		Destroy(gameObject);
+	}
+
+	void Update()
+	{
+		transform.Rotate(Vector3.forward, Time.deltaTime * 40f);
+
+		transform.position += Vector3.up * Mathf.Sin(Time.time * 5f) * Time.deltaTime;
 	}
 }
